cc7ad6db58a803a944304678d96939014fa1b014
Pavan <pavanmishra@gmail.com>
2014-10-09 15:04:27 +0530

    wsgi adaptor added

--- ripple/adaptors/__init__.py
+++ ripple/adaptors/__init__.py
@@ -1,6 +1,1 @@
-def wsgi(webi_app):
-  def wsgi_app(environ, start_response):
-    status, headers, body = webi_app(environ)
-    start_response(status, [(header, value) for header, value in headers.items()])
-    return body
-  return wsgi_app
+from .wsgi import wsgi
---
+++ ripple/adaptors/wsgi.py
@@ -0,0 +1,25 @@
+status_codes = {
+  200: 'OK'
+}
+
+def webi_2_wsgi_status(status):
+  '''
+
+  given an integer status code used by webi, return wsgi compatible status code with description
+
+  >>> webi_2_wsgi_status(200)
+  '200 OK'
+  '''
+  return '%d %s' % (status, status_codes[status])
+
+def wsgi(webi_app):
+  ''' a wsgi adaptor for webi '''
+  def wsgi_app(environ, start_response):
+    status, headers, body = webi_app(environ)
+    start_response(webi_2_wsgi_status(status), [(header, value) for header, value in headers.items()])
+    return [chunk.encode() for chunk in body]
+  return wsgi_app
+
+if __name__ == '__main__':
+  import doctest
+  doctest.testmod()
